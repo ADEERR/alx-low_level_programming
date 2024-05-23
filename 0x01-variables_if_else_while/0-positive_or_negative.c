3e199434773006cb5430abe196cf2e3f425101ab
ADEERR <sattourarida@gmail.com>
2024-05-23 13:46:22 -0700

    pos is negat

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+* main - check  point if is negative, positive, or zero.
+* Return:0
+*/
 int main(void)
 {
 	int n;
@@ -12,16 +15,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf("%d is negative \n", n);
+		printf("%d is negative\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero \n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is positive \n", n);
+		printf("%d is positive\n", n);
 	}
 	/* your code goes there */
 	return (0);
 }
+
